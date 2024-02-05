9b9dd84fabca55f9668a90185a7081c10a7735f8
Nofunzoner <89599752+Nofunzoner@users.noreply.github.com>
2024-02-05 05:32:05 -0800

    Server working
    Finally got server working, fixed async issue.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const path = require('path')
+const http = require('http');
+const assert = require('assert');
+
 const uri = "mongodb+srv://klappin:Y0QfYVlS616m7LSG@cluster0.xecnjnt.mongodb.net/?retryWrites=true&w=majority";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -8,16 +14,30 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
 async function run() {
-    try {
-      const database = client.db('Slitherlake');
-      const maps = database.collection('Maps');
+  try {
+        const database = client.db('Slitherlake');
+        const maps = database.collection('Maps');
   
-      const query = { author: 'Taylor' };
-      const map = await maps.findOne(query);
-      console.log(map);
+        const query = { author: 'Taylor' };
+        var map = await maps.findOne(query); 
     } finally {
       await client.close();
     }
+  return map;
   }
-run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  run().then((results) => {
+    res.json(results);
+  })
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+
